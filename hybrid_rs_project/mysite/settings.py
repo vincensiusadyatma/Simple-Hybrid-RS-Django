fa5d261022215c692de916018d45f791c3742e96
vincenciusadyatma <adyatmav@gmail.com>
2025-06-04 12:02:06 +0700

    feat: create vercell config

--- hybrid_rs_project/mysite/settings.py
+++ hybrid_rs_project/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^fdv-&npk&&_#prmbz_-3fj0n-5bctlbwknq*ewdrhu&tc!=iv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
